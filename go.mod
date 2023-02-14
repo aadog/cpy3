087fc34e42dfa290f48673c8663edd150ae4f2d3
aadog <97077088@qq.com>
2023-02-15 02:46:38 +0800

    update

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aadog/cpy3
+
+go 1.20
+
+require github.com/ying32/liblclbinres v0.0.0-20220111074620-df670ba4baf4
--- main.go
+++ main.go
@@ -1,21 +1,23 @@
-package main
+package cpy3
 
-import (
-	"fmt"
-	_ "github.com/aadog/cpy3/alib"
-	"github.com/aadog/cpy3/cpy/api"
-)
-
-func main() {
-
-	api.Py_Initialize()
-	u := api.PyUnicode_FromString("ff")
-	fmt.Println(api.PyUnicode_AsUTF8(u))
-	//api.Py_SetProgramName("xxxx")
-	//api.Py_SetPath("aaa")
-	//api.Py_SetPythonHome("bb")
-	fmt.Println(api.Py_GetProgramName())
-	fmt.Println(api.Py_GetPath())
-	fmt.Println(api.Py_GetPythonHome())
-	api.Py_Finalize()
-}
+//package main
+//
+//import (
+//	"fmt"
+//	_ "github.com/aadog/cpy3/alib"
+//	"github.com/aadog/cpy3/cpy/api"
+//)
+//
+//func main() {
+//
+//	api.Py_Initialize()
+//	u := api.PyUnicode_FromString("ff")
+//	fmt.Println(api.PyUnicode_AsUTF8(u))
+//	//api.Py_SetProgramName("xxxx")
+//	//api.Py_SetPath("aaa")
+//	//api.Py_SetPythonHome("bb")
+//	fmt.Println(api.Py_GetProgramName())
+//	fmt.Println(api.Py_GetPath())
+//	fmt.Println(api.Py_GetPythonHome())
+//	api.Py_Finalize()
+//}
